98bb2bc06069a63cfd4c71d0804b0a3257d0d759
mike dupont <mike.dupont@introspector.local>
2025-12-02 23:01:41 -0500

    Fix(addr2line): Adapt smallvec::IntoIter usage to new API

--- src/lib.rs
+++ src/lib.rs
@@ -53,7 +53,7 @@ use crate::unit::{ResUnit, ResUnits, SupUnits};
 #[cfg(feature = "smallvec")]
 mod maybe_small {
     pub type Vec<T> = smallvec::SmallVec<T, 16>;
-    pub type IntoIter<T> = smallvec::IntoIter<[T; 16]>;
+    pub type IntoIter<T> = smallvec::IntoIter<T, 16>;
 }
 #[cfg(not(feature = "smallvec"))]
 mod maybe_small {
